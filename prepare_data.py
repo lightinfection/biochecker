54e2a30122cad44467bfaa647f8808d69a6e0568
lightinfection <lightinfected@hotmail.com>
2023-08-15 18:31:43 +0800

    dockerfile

---
+++ prepare_data.py
@@ -0,0 +1,106 @@
+import os 
+import concurrent.futures
+import time
+import numpy as np
+from PIL import Image
+
+class aug_data():
+
+    def __init__(self, path_ctrl, path_cm):
+        self.imgs_0 = [os.path.join(path_ctrl, pic)for pic in os.listdir(path_ctrl)]
+        self.imgs_1 = [os.path.join(path_cm, pic) for pic in os.listdir(path_cm)]
+        self.aug_imgs_0 = []
+        self.aug_imgs_1 = []
+        self._cropnum = 4
+        self._width = 0
+        self._height = 0
+
+    def _fopen(self, im_f):
+        pic = Image.open(im_f)
+        if self._width!=0 and self._height!=0:
+            return pic.convert("L")           
+        self._width = pic.size[0]
+        self._height = pic.size[1]
+        return pic.convert("L")
+    
+    def _crop(self, im):
+        item_width = (self._width/self._cropnum)
+        item_height = (self._height/self._cropnum)
+        box_list = []
+        for i in range(self._cropnum):
+            for j in range(self._cropnum):
+                if i == self._cropnum-1 and j == self._cropnum-1:
+                    continue
+                box = (i*item_width,j*item_height,(i+1)*item_width,(j+1)*item_height)
+                box_list.append(box)
+        return [im.crop(box) for box in box_list]
+    
+    def _rotate_flip(self, im):
+        operations = [Image.FLIP_TOP_BOTTOM, Image.FLIP_LEFT_RIGHT, Image.ROTATE_180]
+        new_list = [im]
+        for op in operations:
+            new_list.append(im.transpose(op))
+        return new_list
+    
+    def _augone(self, pic0):
+        pic = self._fopen(pic0)
+        buf0 = self._crop(pic)
+        temp = []
+        for buf in buf0:
+            temp += self._rotate_flip(buf)
+        return temp
+    
+    def _start_multi(self):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results_0 = executor.map(self._augone, self.imgs_0)
+            results_1 = executor.map(self._augone, self.imgs_1)
+        for result_0 in results_0:
+            self.aug_imgs_0 += result_0
+        for result_1 in results_1:
+            self.aug_imgs_1 += result_1
+
+    def _start(self):
+        for i in self.imgs_0:
+            x = self._augone(i)
+            self.aug_imgs_0 += x
+        for j in self.imgs_1:
+            y = self._augone(j)
+            self.aug_imgs_1 += y
+
+class train_test_split(aug_data):
+
+    def __init__(self, train_ratio=0.7, test_ratio=0.1, path_ctrl=None, path_cm=None):
+        super().__init__(path_ctrl=path_ctrl, path_cm=path_cm)
+        self.train_ratio = float(train_ratio)
+        self.test_ratio = float(test_ratio)
+        self._start_multi()
+        label0 = [ 0 for _ in range(len(self.aug_imgs_0))]
+        label1 = [ 1 for _ in range(len(self.aug_imgs_1))]
+        self._total = list(zip(self.aug_imgs_0 + self.aug_imgs_1, label0 + label1))
+
+    def _split(self):
+        rng1 = np.random.default_rng(seed=14)
+        rng2 = np.random.default_rng(seed=28)
+        tot = len(self._total)
+        train_index = list(rng1.choice(tot, size=int(self.train_ratio*tot), replace=False))
+        self._train_set, temp_left = self._minus(total=self._total, tobermed=train_index)
+        test_index = list(rng2.choice(len(temp_left), size=int(self.test_ratio*tot), replace=False))
+        self._test_set, self._validation_set = self._minus(total=temp_left, tobermed=test_index)
+        
+    @staticmethod
+    def _minus(total, tobermed):
+        buf = []
+        for i in sorted(tobermed, reverse=True):
+            buf.append(total[i])
+            del total[i]
+        return buf, total
+
+def main():
+    test = train_test_split(path_ctrl="/home/ibot/Desktop/image/Original/Ctrl",path_cm="/home/ibot/Desktop/image/Original/CM")
+    start = time.time()
+    test._split()
+    print(test._train_set[0][0])
+    print("spending", float(time.time()-start), "s", len(test._train_set), len(test._validation_set), len(test._test_set))
+
+if __name__ == "__main__":
+    main()
---
+++ train.py
@@ -0,0 +1,130 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+import numpy as np
+
+class LossHistory(tf.keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = {'batch':[], 'epoch':[]}
+        self.accuracy = {'batch':[], 'epoch':[]}
+        self.val_loss = {'batch':[], 'epoch':[]}
+        self.val_acc = {'batch':[], 'epoch':[]}
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses['batch'].append(logs.get('loss'))
+        self.accuracy['batch'].append(logs.get('accuracy'))
+        self.val_loss['batch'].append(logs.get('val_loss'))
+        self.val_acc['batch'].append(logs.get('val_accuracy'))
+
+    def on_epoch_end(self, batch, logs={}):
+        self.losses['epoch'].append(logs.get('loss'))
+        self.accuracy['epoch'].append(logs.get('accuracy'))
+        self.val_loss['epoch'].append(logs.get('val_loss'))
+        self.val_acc['epoch'].append(logs.get('val_accuracy'))
+
+    def loss_plot(self, loss_type):
+        iters = range(len(self.losses[loss_type]))
+        if loss_type == 'epoch':
+            # val_acc
+            plt.subplot(1,2,1)
+            plt.plot(iters, self.val_acc[loss_type], 'b', label='val acc')
+            plt.plot(iters, self.accuracy[loss_type], 'r', label='train acc')
+            plt.grid(True)
+            plt.xlabel(loss_type)
+            plt.ylabel('acc')
+            plt.legend(loc="lower right")
+            # val_loss
+            plt.subplot(1,2,2)
+            plt.plot(iters, self.val_loss[loss_type], 'k', label='val loss')
+            plt.plot(iters, self.losses[loss_type], 'g', label='train loss')
+            plt.grid(True)
+            plt.xlabel(loss_type)
+            plt.ylabel('loss')
+            plt.legend(loc="upper right")
+    
+    def show(self):
+        plt.show()
+        
+    def save(self, path, dpi):
+        plt.savefig(fname=path, dpi=dpi)
+
+class model():
+
+    def __init__(self, train, validation, test):
+        self._train_set = self.load_img(train)
+        self._validation_set = self.load_img(validation)
+        self._test_set = self.load_img(test)
+        self.history = LossHistory()
+        inputs, outputs = self._model_base(shp=self._train_set.shape[1:])
+        self._model = keras.Model(inputs, outputs)
+    
+    def load_img(self, dataset):
+        x = [tf.keras.utils.img_to_array(i[0]) for i in dataset]
+        y = [i[1] for i in dataset]
+        return (np.array(x), np.array(y))
+
+    def _model_base(shp, w=None, h=None):
+        w_i = keras.initializers.GlorotNormal(seed=1000)
+        b_i = keras.initializers.Zeros()
+
+        inputs = keras.Input(shape=(256, 340, 1))
+        x = layers.Resizing(256,256)(inputs)
+        x = layers.Rescaling(1./255)(x)
+        x = layers.Conv2D(filters=32, kernel_size=(5,5), strides=(1,1), activation='relu', input_shape=(256,256,1), kernel_initializer=w_i, bias_initializer=b_i)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.MaxPool2D(pool_size=(3,3), strides=(2,2))(x)
+
+        x_skip = x
+        x_skip = layers.Conv2D(filters=64, kernel_size=(1,1), strides=(1,1),kernel_initializer=w_i, bias_initializer=b_i)(x_skip)
+        x = layers.Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same", kernel_initializer=w_i, bias_initializer=b_i)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.add([x, x_skip])
+        x = layers.MaxPool2D(pool_size=(3,3), strides=(2,2))(x)
+
+        x_skip = x
+        x_skip = layers.Conv2D(filters=128, kernel_size=(1,1), strides=(1,1),kernel_initializer=w_i, bias_initializer=b_i)(x_skip)
+        x = layers.Conv2D(filters=128, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same", kernel_initializer=w_i, bias_initializer=b_i)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.add([x, x_skip])
+        x = layers.MaxPool2D(pool_size=(3,3), strides=(2,2))(x)
+
+        x_skip = x
+        x = layers.Conv2D(filters=128, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same", kernel_initializer=w_i, bias_initializer=b_i)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.add([x, x_skip])
+        x = layers.MaxPool2D(pool_size=(3,3), strides=(2,2))(x)
+        x = layers.Flatten()(x)
+        x = layers.Dense(1024, activation='relu')(x)
+        outputs = layers.Dense(1, activation='sigmoid')(x)
+
+        return inputs, outputs
+
+    def _compile(self):
+        self._model.compile(loss="binary_crossentropy",
+                    optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3,epsilon=1e-5),
+                    metrics =['accuracy'])
+    
+    def _fit(self, batch_size, epochs):
+        self._model.fit(self._train_set,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    validation_data=self._validation_set,
+                    callbacks=[self.history])
+    
+    def view_learning_curve(self):
+        self.history.loss_plot('epoch')
+        self.history.show()
+    
+    def save_learning_curve(self, path, dpi):
+        self.history.loss_plot('epoch')
+        self.history.save(path, dpi)
+
+    def predict(self):
+        cm =np.array(tf.math.confusion_matrix(predictions=
+                                              np.array(tf.round(model.predict(self._test_set[0]))), 
+                                              labels=self._test_set[1], 
+                                              num_classes=2))
+        self.acc = float((cm[0,0]+cm[1,1])/(cm[0,0]+cm[0,1]+cm[1,0]+cm[1,1]))
+        print("test_acc", self.acc)
+
